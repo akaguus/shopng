71c23667514d4ddc866e2b2a1bafe80ef2075300
akaguus <akaguus@gmail.com>
2014-06-16 14:44:14 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopng::Application.config.secret_token = '504c0c702d0557dccb1dd700d6108bdb08fb61f5b99be95bd86391f0c6e61376d25bee9abd47ddce4c4f23bdd2e42033350411e46d83296171386da1fbd7c61f'
